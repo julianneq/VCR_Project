9e238fba570e42508230e5efce67c9012b9816db
Julianne Quinn <julianneq@users.noreply.github.com>
2015-06-08 16:38:28 -0400

    Create convertDBFtoCSV.py

---
+++ Soil/convertDBFtoCSV.py
@@ -0,0 +1,19 @@
+import csv
+import arcpy as ap
+
+def convertDBFtoCSV(dbf):
+    '''Converts database file of average AggLevel soil data to a csv'''
+    table = dbf
+    outfile = dbf[0:-4] + ".csv"
+
+    fields = ap.ListFields(table)
+    field_names = [field.name for field in fields]
+    with open(outfile,'wb') as f:
+        dw = csv.DictWriter(f,field_names)
+        dw.writeheader()
+
+        with ap.da.SearchCursor(table,field_names) as cursor:
+            for row in cursor:
+                dw.writerow(dict(zip(field_names,row)))
+
+    return None
